06e20ea1b857a63eca95c77cf360251b204d1ff4
haekalalawy15 <32903358+haekalalawy15@users.noreply.github.com>
2020-05-20 20:13:32 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const sql = require('mssql')
-const hostname = '10.199.14.46';
+const hostname = 'localhost';
 const port = 8001;
 
 //CORS Middleware
